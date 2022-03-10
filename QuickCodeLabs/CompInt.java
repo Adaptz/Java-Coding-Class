2c16f4640c6ea574784668e8e7c41d8b761f8751
Adaptz <84076017+Adaptz@users.noreply.github.com>
2022-03-09 23:46:42 -0600

    Create CompInt.java

---
+++ QuickCodeLabs/CompInt.java
@@ -0,0 +1,3 @@
+/*Compound Interest. Complete the main method that produces the given output
+for this compounding interest problem. This interest should be compounded for 40 years.
+Use the following equation to : principal = ((principal + (12 * amountMonth))* (1+rate)).*/
