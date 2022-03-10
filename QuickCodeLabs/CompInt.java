1e62ba1cf1d46ff710a50e2f501606a1cdb9d2e7
Adaptz <84076017+Adaptz@users.noreply.github.com>
2022-03-10 08:36:09 -0600

    Update CompInt.java

--- QuickCodeLabs/CompInt.java
+++ QuickCodeLabs/CompInt.java
@@ -1,3 +1,22 @@
-/*Compound Interest. Complete the main method that produces the given output
-for this compounding interest problem. This interest should be compounded for 40 years.
-Use the following equation to : principal = ((principal + (12 * amountMonth))* (1+rate)).*/
+public class CompInt
+{
+    public static void main(String[] args) 
+    {
+
+        double amountMonth = 500.0; // amount deposit each month
+        double principal = 2000.0; // initial amount before interest
+        double rate = 0.06; // interest rate
+        System.out.println("Hello World");
+        for(int years = 1; years <= 40; years++)
+        {
+
+            principal = ((principal + (12 * amountMonth))* (1+rate));
+
+            if(years == 1)
+            System.out.printf("Compunded Interest:\n\n%5s %20s\n", "Year", "Principal Interest");
+
+            System.out.printf("%5d %,20.2f\n", years, principal);
+
+        }
+    }
+}
