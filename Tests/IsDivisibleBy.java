a623057bb1a0d18a3b090c9a13af2daff5fb8385
Adaptz <84076017+Adaptz@users.noreply.github.com>
2022-03-11 23:44:51 -0600

    Update IsDivisibleBy.java

--- Tests/IsDivisibleBy.java
+++ Tests/IsDivisibleBy.java
@@ -1,31 +1,48 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class IsDivisibleBy {
     public static void main(String[] args)
     {
         int[] array;
-        array = new int[10000];
-        int index = 0;
+        array = new int[100000];
+        int number = 1;
+        int loopnum = 1;
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter the number to check what it's divisible by (checks from 2-10000): ");
-        int number = input.nextInt();
-
-        System.out.printf("%d is divisible by: ", number);
-
-        for(int check = 1; check < array.length; check++)
+        while(number != 0)
         {
-            if( number % check == 0)
+            int index = 0;
+
+            if(loopnum == 1)
+            System.out.print("\n************************** Enter a 0 to exit **************************");
+
+            System.out.println("\n\nEnter the number to check what it's divisible by (checks from 2-100000): ");
+            number = input.nextInt();
+            if(number == 0)
+            System.exit(0); 
+            
+            System.out.printf("%d is divisible by: ", number);
+
+            for(int check = 1; check < array.length; check++)
             {
-                array[index] = check;
-                System.out.printf(" %d ", array[index]);
-                index++;
+                if( number % check == 0)
+                {
+                    array[index] = check;
+                    System.out.printf(" %d ", array[index]);
+                    index++;
+                }
             }
-        }
-        if(array[3] == 0)
-        {
-            System.out.printf("\n%d is a prime number", number, number);
+
+            if(array[2] == 0)
+            {
+                System.out.printf("\n%d is a prime number", number, number);
+            }
+
+            Arrays.fill(array, 0);
+            loopnum++;
+            
         }
     }
     
